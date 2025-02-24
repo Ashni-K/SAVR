44808e387f57a62ba7aef66a0baf3c61cbfd39d1
Spoorthi Pyarilal <spoorthipyarilal@n3-50-31.dhcp.drexel.edu>
2025-02-24 00:51:43 -0500

    Pantry page allows ingredient selection and selected ingredients appear on the page - try 2

--- app/src/main/java/com/example/savr/MainActivity.kt
+++ app/src/main/java/com/example/savr/MainActivity.kt
@@ -5,12 +5,6 @@ import androidx.activity.ComponentActivity
 
 import android.content.Intent
 import android.widget.Button
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import com.example.savr.ui.theme.SAVRTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
