0283102961fdbe8e404ad11c3dd41f00cc3bba66
najda <ddollani24@scienceleadership.org>
2025-05-20 23:49:23 -0400

    Profile picture exists and can be changed by user

--- app/src/main/java/com/example/savr/MeActivity.kt
+++ app/src/main/java/com/example/savr/MeActivity.kt
@@ -1,15 +1,32 @@
 package com.example.savr
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MeActivity : ComponentActivity(){
+
+    private val pickImageLauncher = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        uri?.let {
+            findViewById<ImageView>(R.id.imageView).setImageURI(it)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.me)
+
+        val profileImage = findViewById<ImageView>(R.id.imageView)
+
+        profileImage.setOnClickListener {
+            pickImageLauncher.launch("image/*")
+        }
 
         // Button navigation
         val pantryButton = findViewById<Button>(R.id.button42)
