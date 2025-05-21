45bdc365bd9eea5bdc40752885d0b99af1a5a4e9
najda <ddollani24@scienceleadership.org>
2025-05-20 22:43:18 -0400

    Me page created

---
+++ app/src/main/java/com/example/savr/MeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.savr
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MeActivity : ComponentActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.me)
+    }
+}
